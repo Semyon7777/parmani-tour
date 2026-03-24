08e1e9f3ca938532e332078b4a52fb9a096a1e69
Semyon_test_accaunt <160015795+Semyon7777@users.noreply.github.com>
2026-03-24 19:52:59 +0400

    css correcting

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,3 +4,5 @@ const supabaseUrl = "https://rllzxsuzaojqnconqwdd.supabase.co";
 const supabaseAnonKey = "sb_publishable_Ti0ZPv-gtutIhzeE_7B8wA_HdeK5WFW";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+//https://rllzxsuzaojqnconqwdd.supabase.co/auth/v1/callback
